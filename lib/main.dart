a64b3a162fdc99227c8bbf0b66adcc36b5eeb155
Dineo Mathibela <dineomathibela161@gmail.com>
2022-05-02 06:47:44 -0500

    #containers_CH2

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,50 @@ void main() {
 
 //StatelessWidget : means it will never change
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Candidate App"),
-      ),
-      body: Center(
-        child: Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            alignment: Alignment.centerLeft,
-            decoration: BoxDecoration(
-              color: Colors.blueGrey,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [
-                Colors.pink,
-                Colors.yellow,
-              ]),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 10,
-                ),
-              ]),
-            child: Text("I am a box",style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),),
-          ),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Candidate App"),
+        ),
+        body: Container(
+          height: 500,
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+            Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          alignment: Alignment.centerLeft,
+            ),
+             Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          alignment: Alignment.centerLeft,
+            ),
+             Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          alignment: Alignment.centerLeft,
+            ),
+             Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          alignment: Alignment.centerLeft,
+            ),
+          ]),
+        ));
   }
 }
